935ce3f2192853c506e368df9cc35ff2697976a8
chris09877 <104131775+chris09877@users.noreply.github.com>
2023-12-27 15:58:30 +0100

    css on create question page done

--- resources/views/createFaq2.blade.php
+++ resources/views/createFaq2.blade.php
@@ -1,36 +1,36 @@
 @extends('layouts.app')
-@section('title', 'Ask Your Question')
-@section('content')
 
-    <h1 class="text-4xl mb-8 text-center ">Create question</h1>
-    
-    <div id="postModal" title="Create Post" class="container mx-auto py-8 mt-5">
-        <form action="{{ route('submitFaq') }}" method="POST" enctype="multipart/form-data" id="post-form"
-            data-route="{{ route('submitFaq') }}">
+@section('title', 'Ask Your Question')
+
+@section('content')
+<div class="flex justify-center items-center min-h-screen">
+    <div class="w-2/3 p-6 bg-white shadow-md rounded-md">
+        <h1 class="text-4xl mb-8 text-center">Create question</h1>
+
+        <form action="{{ route('submitFaq') }}" method="POST" enctype="multipart/form-data" id="post-form">
             @csrf
+
             <div class="mb-4">
                 <label for="Title" class="block text-gray-700 text-sm font-bold mb-2">Title:</label>
                 <input type="text" id="Title" name="title" class="border rounded-md py-2 px-3 w-full">
             </div>
-    
+
             <div class="mb-4">
                 <label for="Content" class="block text-gray-700 text-sm font-bold mb-2">Content:</label>
-                <textarea id="Content" name="text" rows="4" cols="50" class="border rounded-md py-2 px-3 w-full"></textarea>
+                <textarea id="Content" name="text" rows="4" class="border rounded-md py-2 px-3 w-full"></textarea>
             </div>
-    
+
             <div class="mb-4">
                 <label for="category" class="block text-gray-700 text-sm font-bold mb-2">Select category:</label>
-                <select type="" id="category" name="categories" class="border rounded-md py-2 px-3">
+                <select id="category" name="categories" class="border rounded-md py-2 px-3 w-full">
                     @foreach($categories as $category)
-                        <option value="{{ $category->id }}">{{ $category->name }}</option>
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
                     @endforeach
                 </select>
             </div>
-    
-            <input type="submit" value="Submit" class=" font-bold py-2 px-4 rounded border">
+
+            <input type="submit" value="Submit" class="font-bold py-2 px-4 rounded border bg-blue-500 text-white hover:bg-blue-700">
         </form>
     </div>
-    
-    
-
+</div>
 @endsection
