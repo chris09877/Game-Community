509299e704fa1076666ab0ebc1270b8a7d756c05
chris09877 <104131775+chris09877@users.noreply.github.com>
2023-12-27 12:58:39 +0100

    nav bar css for all pages beside welcome done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,10 +8,13 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name') }} - @yield('title')</title>
+    <title>@yield('title') -{{ config('app.name') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -19,172 +22,82 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+        
+        li {
+  list-style-type: none;
+}
+
+    </style>
+    
 </head>
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
+        <nav class="bg-white shadow-md">
+            <div class="container mx-auto">
+                <div class="flex justify-between items-center py-3">
+                    <a href="{{ url('/') }}" class="text-xl font-bold text-gray-600">{{ config('app.name', 'Laravel') }}</a>
+                    <button class="block lg:hidden focus:outline-none">
+                        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+                        </svg>
+                    </button>
+                    <div class="hidden lg:flex lg:items-center lg:w-auto lg:space-x-4">
                         @auth
-
-                        <li class="nav-item">
                             <a href="{{ route('home') }}" class="nav-link">Dashboard</a>
-                        </li>
-
-                        <li class="nav-item">
                             <a href="{{ route('faq') }}" class="nav-link">FAQ</a>
-                        </li>
-                        <li class="nav-item">
                             <a href="{{ route('contact') }}" class="nav-link">Contact Us</a>
-                        </li>
-                        @if(auth()->user()->admin)
-                        <li class="nav-item">
-                            <a href="{{ route('settings') }}" class="nav-link">Settings</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('category') }}" class="nav-link">Categories</a>
-                        </li>
-                        @endif
-                        {{-- <li class="nav-item dropdown" >
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a href="{{ route('profile') }}" class="dropdown-item">Profile</a>
-
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                             document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li> --}}
+                            @if(auth()->user()->admin)
+                                <a href="{{ route('settings') }}" class="nav-link">Settings</a>
+                                <a href="{{ route('category') }}" class="nav-link">Categories</a>
+                            @endif
                         @endauth
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <!-- Right Side Of Navbar -->
-<ul class="navbar-nav ms-auto">
-    @guest
-        @if (Route::currentRouteName() == 'login')
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-            </li>
-            <li class="nav-item">
-                <a href="{{ route('faq') }}" class="nav-link">{{ __('FAQ') }}</a>
-            </li>
-            <li class="nav-item">
-                <a href="{{ route('contact') }}" class="nav-link">{{ __('Contact Us') }}</a>
-            </li>
-        @elseif (Route::currentRouteName() == 'register')
-            <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-            </li>
-            <li class="nav-item">
-                <a href="{{ route('faq') }}" class="nav-link">{{ __('FAQ') }}</a>
-            </li>
-            <li class="nav-item">
-                <a href="{{ route('contact') }}" class="nav-link">{{ __('Contact Us') }}</a>
-            </li>
-        @endif
-        
-    @endguest
-    @auth
-    <li class="nav-item dropdown" >
-        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-            {{ Auth::user()->name }}
-        </a>
-
-        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-            <a href="{{ route('profile') }}" class="dropdown-item">Profile</a>
-
-            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-            </a>
-
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                @csrf
-            </form>
-        </div>
-    </li>
-    @endauth
-
-                    {{-- <ul class="navbar-nav ms-auto">
                         @guest
-                        @if (Route::has('login'))
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('faq') }}" class="nav-link">{{ __('FAQ') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('contact') }}" class="nav-link">{{ __('Contact Us') }}</a>
-                        </li>
-
-
-                        @elseif (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('faq') }}" class="nav-link">{{ __('FAQ') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('contact') }}" class="nav-link">{{ __('Contact Us') }}</a>
-                        </li> --}}
-                        {{-- <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li> --}}
-
-                        {{-- @else --}}
-                        {{-- <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a href="{{ route('profile') }}" class="dropdown-item">Profile</a>
-
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                            @if(Route::currentRouteName() == 'login')
+                                <a href="{{ route('register') }}" class="nav-link">Register</a>
+                                <a href="{{ route('faq') }}" class="nav-link">FAQ</a>
+                                <a href="{{ route('contact') }}" class="nav-link">Contact Us</a>
+                            @elseif(Route::currentRouteName() == 'register')
+                                <a href="{{ route('login') }}" class="nav-link">Login</a>
+                                <a href="{{ route('faq') }}" class="nav-link">FAQ</a>
+                                <a href="{{ route('contact') }}" class="nav-link">Contact Us</a>
+                            @endif
+                           
+                        @endguest
+                        @auth
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
                                 </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li> --}}
-                        {{-- @endif
-                        @endguest --}}
-                        
-                    </ul>
+                                <div class="hidden lg:flex lg:items-center lg:w-auto lg:space-x-4">
+                                    
+                                        <!-- ... -->
+                                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                            <a href="{{ route('profile') }}" class="dropdown-item">
+                                                <span class="list-none">Profile</span>
+                                            </a>
+                                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                                onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                                <span class="list-none">{{ __('Logout') }}</span>
+                                            </a>
+                                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                @csrf
+                                            </form>
+                                        </div>
+                                    
+                                </div>
+                                
+                                
+                            </li>
+                        @endauth
+                    </div>
+                    
                 </div>
             </div>
         </nav>
-
+        
         <main class="py-4">
             @yield('content')
         </main>
