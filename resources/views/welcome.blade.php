a4f1b2b0d50eb200beb72671be97f23fadd0b38d
chris09877 <104131775+chris09877@users.noreply.github.com>
2024-01-11 22:27:45 +0100

    logo added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,9 @@
         @section('content')
         <div class="text-center">
             <!-- Image centered in the middle of the page -->
-            <img src="path_to_your_image.jpg" alt="Community Game EhB" class="mx-auto mb-8">
-    
+            <div class="mx-auto mb-8">
+            <img src="{{ asset('storage/ehb-logo.jpg') }}" alt="Community Game EhB" class="mx-auto mb-8">
+        </div>
             <!-- Large h1 heading -->
             <h1 class="text-white text-4xl font-bold">Community Game EHB</h1>
         </div>
